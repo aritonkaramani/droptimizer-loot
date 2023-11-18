cc9666531eb412405ae2abe50ee5e0efea494187
NAskjaer <askjaer.ni@hotmail.com>
2023-11-18 16:24:02 +0100

    updated upload.py

--- src/upload.py
+++ src/upload.py
@@ -9,11 +9,7 @@ scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/au
 
 credentials = ServiceAccountCredentials.from_json_keyfile_name('src/client_secret.json', scope)
 client = gspread.authorize(credentials)
-
-spreadsheet_normal = client.open('Northstar Sims Normal')
-spreadsheet_heroic = client.open('Northstar Sims Heroic')
-spreadsheet_mythic = client.open('Northstar Sims Mythic')
-
+spreadsheet = client.open('Northstar Sims')
 
 def ImportCsv(csvFile, sheet, tabname):
     wks = sheet.worksheet(tabname)
@@ -40,13 +36,6 @@ def ImportCsv(csvFile, sheet, tabname):
 
 def directoryFinder(dir):
     directory = dir
-    difficulty = directory.split('_')[1]
-    if difficulty == "normal": 
-        spreadsheet = spreadsheet_normal
-    elif difficulty == "heroic":
-        spreadsheet = spreadsheet_heroic   
-    elif difficulty == "mythic": 
-        spreadsheet = spreadsheet_mythic
 
     # below loops each file in that directory.
     for filename in os.listdir(directory):
@@ -56,23 +45,13 @@ def directoryFinder(dir):
         tabname = Path(f).stem
         # Checking if it is a file
         if os.path.isfile(f):
-            # Creating the new spreadsheet tab
-            # print(spreadsheet.worksheet(f'{filename}'))
             worksheet = spreadsheet.worksheet(f'{tabname}')
-            # else :
-            #     print("Aint working")
-            #     worksheet = spreadsheet.add_worksheet(f'{filename}', rows = 1000, cols = 100)
-
             print("Trying to update: " + tabname)
             ImportCsv(f, spreadsheet, tabname)
 
-        
 if __name__ == '__main__':
-    directoryFinder('src/generated_heroic')
-    time.sleep(60)
-    directoryFinder('src/generated_mythic')
-    time.sleep(60)
-    directoryFinder('src/generated_normal')
+    directoryFinder('src/data')
+
 
 
             
